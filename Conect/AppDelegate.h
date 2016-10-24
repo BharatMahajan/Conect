a178ea305a526b7f555086552406317fc5c01678
Bharat Mahajan <bharatmahajan@misbrdmac1.local>
2016-10-24 12:06:28 +0530

    Initial Commit

---
+++ Conect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Conect
+//
+//  Created by Bharat Mahajan on 10/24/16.
+//  Copyright © 2016 Bharat Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Conect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Conect
+//
+//  Created by Bharat Mahajan on 10/24/16.
+//  Copyright © 2016 Bharat Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
